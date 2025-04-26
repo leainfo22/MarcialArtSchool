84e1ad7e84847fd5456403da90ead1cf5f6647a4
leainfo22 <leandro.vasquez.mora@gmail.com>
2025-04-27 00:33:24 +1000

    Update appsettings.json with logging configuration and connection string.

---
+++ MarcialArtSchool/MartialArtSchool.Infrastructure/DbContext/DapperDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System.Data;
+
+namespace MartialArtSchool.Infrastructure.DbContext;
+public class DapperDbContext
+{
+    private readonly IConfiguration _configuration;
+    private readonly IDbConnection _connection;
+    public DapperDbContext(IConfiguration configuration)
+    {
+        _configuration = configuration;
+        string? connectionString = _configuration.GetConnectionString("SqlConnection");
+        _connection = new SqlConnection(connectionString);
+
+    }
+    public IDbConnection DbConnection => _connection;
+}
+
+
